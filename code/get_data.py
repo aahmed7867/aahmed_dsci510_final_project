fda9d6a21844fcd86704c2ff749dbbcf8471afb2
Abdullah Ahmed <110873703+aahmed7867@users.noreply.github.com>
2022-11-30 23:11:37 -0800

    Data Collection Code ver-1

---
+++ code/get_data.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from meteostat import Point, Daily
+
+los_a = Point(34.0167, -118.2833, 70)
+
+new_delhi = Point(28.5833, 77.2, 70)
+
+london = Point(51.5167, -0.1167, 70)
+
+sydney = Point(-33.9, 151.2, 70)
+
+rio_de_jan = Point(-22.9, -43.1667, 70)
+
+cairo = Point(30.1333, 31.4, 70)
+
+
