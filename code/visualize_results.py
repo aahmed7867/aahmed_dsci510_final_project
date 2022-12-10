897e255b71ce0fa3ccc24b938e8e4b233f7c4f3c
Abdullah Ahmed <110873703+aahmed7867@users.noreply.github.com>
2022-12-09 17:03:07 -0800

    Visualizing the results ver-1

---
+++ code/visualize_results.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import matplotlib.dates as mdates
+import datetime
+
+
+def line_plot(df_dict, year, city, columns):
+    date_formatter = mdates.DateFormatter("%Y-%m")
+    date_locator = mdates.MonthLocator(interval=2)
+    dates = [datetime.datetime.strptime(
+        d, "%Y-%m-%d") for d in df_dict[year][city]['time'].values.tolist()]
+    fig = plt.figure(figsize=(14, 9))
+    ax = plt.gca()
+    ax.xaxis.set_major_formatter(date_formatter)
+    ax.xaxis.set_major_locator(date_locator)
+    for col in columns:
+        plt.plot(dates, df_dict[year][city][col].values.tolist(), label=col)
+    plt.tight_layout()
+    plt.xlabel('Time (Year - Month)')
+    plt.ylabel('Values')
+    fig.autofmt_xdate()
+    plt.legend()
+    if year == 'alltime':
+        plt.title(', '.join(columns) +
+                  ' Variation over time 2018, 2019, 2020 in '+city)
+        plt.savefig('results/'+', '.join(columns) +
+                    ' Variation over time 2018, 2019, 2020 in '+city+'.png', bbox_inches='tight')
+    else:
+        plt.title(', '.join(columns)+' Variation over time ' +
+                  str(year)+' in '+city)
+        plt.savefig('results/'+', '.join(columns) +
+                    ' Variation over time '+str(year)+' in '+city+'.png', bbox_inches='tight')
+    plt.show()
