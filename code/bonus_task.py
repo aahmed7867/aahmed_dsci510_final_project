3b5d7e305457e1554017de8872b95f59a01d3c45
Abdullah Ahmed <110873703+aahmed7867@users.noreply.github.com>
2022-12-09 16:34:27 -0800

    Comments added to the Bonus Task file

--- code/bonus_task.py
+++ code/bonus_task.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime
 
+# reading the data file and creating the dataframe
 df = pd.read_csv('data/Cairo_weather_data_2018.csv')
 
 dates_main = []
@@ -11,22 +12,27 @@ y2_main = []
 y3_main = []
 
 fig, axes = plt.subplots(figsize=(12, 8))
+# formatter - shows the year and month only
 date_formatter = mdates.DateFormatter("%Y-%m")
+# the labels are shown in an interval of two months
 date_locator = mdates.MonthLocator(interval=1)
+# adding the formatter to the figure
 axes.xaxis.set_major_formatter(date_formatter)
 axes.xaxis.set_major_locator(date_locator)
 
 for i in range(120):
+    # plotting the data in the figure as a simulation
     dates_main.append([datetime.datetime.strptime(d, "%Y-%m-%d")
                        for d in df['time'].values.tolist()][i])
     y1_main.append(df['tmin'].values.tolist()[i])
     y2_main.append(df['tavg'].values.tolist()[i])
     y3_main.append(df['tmax'].values.tolist()[i])
-    axes.clear()   
+    axes.clear()   # clearing the figure
     fig.autofmt_xdate()
     axes.plot(dates_main, y1_main, 'r')
     axes.plot(dates_main, y2_main, 'g')
     axes.plot(dates_main, y3_main, 'b')
+    # defining plot parameters
     axes.set_xlabel('Date', fontsize=18)
     axes.set_ylabel('Temperature value', fontsize=18)
     axes.set_title('Temperature variation in Cairo city in January-May 2018', fontsize=20)
@@ -37,7 +43,7 @@ for i in range(120):
                   xy=(dates_main[-1], y2_main[-1]+0.5))
     axes.annotate('tmax-'+str(y3_main[-1]),
                   xy=(dates_main[-1], y3_main[-1]+0.5))
-    plt.pause(0.5)  
+    plt.pause(0.5)  # pausing the ploting for a short time
 
 plt.savefig('results/bonus_task_plot.png', bbox_inches='tight')
 plt.show()
