658d8f81bb63f9301f754e8653fffab3d5c01a96
Abdullah Ahmed <110873703+aahmed7867@users.noreply.github.com>
2022-12-09 18:20:16 -0800

    Comments added to the main.py file

--- code/main.py
+++ code/main.py
@@ -4,10 +4,19 @@ from visualize_results import *
 
 
 def main():
-    dfs = read_csv_files()  
+    '''
+    This function combines all the subprocesses into a single main process, using previously defined functions.
+    '''
+    # load_data()   # to load the data from the API
+    dfs = read_csv_files()  # reading all the CSV files and making the dataframes dictionary
+    # combining dataframes and making the 'alltime' dictionaries
     dfs = combine_df_city(dfs)
 
+    # as an example, the following operations are simulated
+
+    # finding maximum records in Cairo city for 'alltime' time range
     print(max_n_param_in_city(dfs, 'tavg', 'Cairo', 'alltime', 10))
+    # making some plots
     line_plot(dfs, 'alltime', 'Los_Angeles', ['tavg', 'wspd'])
     correlation_plot(dfs, 2020, 'Los_Angeles')
     scatter_plot(dfs, 2020, 'Sydney', ['tmin', 'tavg', 'tmax'])
